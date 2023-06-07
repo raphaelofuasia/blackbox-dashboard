614e96a598c873f657a7be7ccf31d6dc357f0be4
John <johnudoumoh63@gmail.com>
2023-06-07 18:38:46 +0100

    .

---
+++ src/views/dashboard/Default/Map.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Grid } from '@mui/material';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+
+const Map = ({ latitude, longitude }) => {
+  const position = [latitude, longitude];
+  const position2 = [6.670473801959922, 3.153438644180987];
+  function ChangeView({ center, zoom = 12 }) {
+    const map = useMap();
+    map.setView(center, zoom);
+    return null;
+  }
+  return (
+    <Grid item lg={12} xs={12} md={8} bg="white" p="10" boxShadow="lg">
+      <MapContainer center={position} zoom={13} scrollWheelZoom={true} style={{ height: '500px' }}>
+        <ChangeView center={position2} />
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </Grid>
+  );
+};
+
+Map.propTypes = {
+  // latitude: PropTypes.float,
+  // longitude: PropTypes.float,
+  // center: PropTypes.number,
+  // zoom: PropTypes.number
+};
+
+export default Map;
---
+++ src/views/dashboard/Default/VibrationCard.js
@@ -0,0 +1,136 @@
+import PropTypes from 'prop-types';
+
+// material-ui
+import { useTheme, styled } from '@mui/material/styles';
+import { Avatar, Box, Grid, Typography } from '@mui/material';
+
+// project imports
+import MainCard from 'ui-component/cards/MainCard';
+import SkeletonTotalOrderCard from 'ui-component/cards/Skeleton/EarningCard';
+
+// assets
+import { MdVibration } from 'react-icons/md';
+// import LocalMallOutlinedIcon from '@mui/icons-material/LocalMallOutlined';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+
+const CardWrapper = styled(MainCard)(({ theme }) => ({
+  backgroundColor: theme.palette.primary.dark,
+  color: '#fff',
+  overflow: 'hidden',
+  position: 'relative',
+  '&>div': {
+    position: 'relative',
+    zIndex: 5
+  },
+  '&:after': {
+    content: '""',
+    position: 'absolute',
+    width: 210,
+    height: 210,
+    background: theme.palette.primary[800],
+    borderRadius: '50%',
+    zIndex: 1,
+    top: -85,
+    right: -95,
+    [theme.breakpoints.down('sm')]: {
+      top: -105,
+      right: -140
+    }
+  },
+  '&:before': {
+    content: '""',
+    position: 'absolute',
+    zIndex: 1,
+    width: 210,
+    height: 210,
+    background: theme.palette.primary[800],
+    borderRadius: '50%',
+    top: -125,
+    right: -15,
+    opacity: 0.5,
+    [theme.breakpoints.down('sm')]: {
+      top: -155,
+      right: -70
+    }
+  }
+}));
+
+// ==============================|| DASHBOARD - TOTAL ORDER LINE CHART CARD ||============================== //
+
+const TotalOrderLineChartCard = ({ isLoading, vibration }) => {
+  const theme = useTheme();
+
+  return (
+    <>
+      {isLoading ? (
+        <SkeletonTotalOrderCard />
+      ) : (
+        <CardWrapper border={false} content={false}>
+          <Box sx={{ p: 2.25 }}>
+            <Grid container direction="column">
+              <Grid item>
+                <Grid container justifyContent="space-between">
+                  <Grid item>
+                    <Avatar
+                      variant="rounded"
+                      sx={{
+                        ...theme.typography.commonAvatar,
+                        ...theme.typography.largeAvatar,
+                        backgroundColor: theme.palette.primary[800],
+                        color: '#fff',
+                        mt: 1
+                      }}
+                    >
+                      <MdVibration />
+                    </Avatar>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item sx={{ mb: 0.75 }}>
+                <Grid container alignItems="center">
+                  <Grid>
+                    <Grid container alignItems="center">
+                      <Grid item>
+                        <Typography sx={{ fontSize: '2.125rem', fontWeight: 500, mr: 1, mt: 1.75, mb: 0.75 }}>{vibration}</Typography>
+                      </Grid>
+                      <Grid item>
+                        <Avatar
+                          sx={{
+                            ...theme.typography.smallAvatar,
+                            cursor: 'pointer',
+                            backgroundColor: theme.palette.primary[200],
+                            color: theme.palette.primary.dark
+                          }}
+                        >
+                          <ArrowDownwardIcon fontSize="inherit" sx={{ transform: 'rotate3d(1, 1, 1, 45deg)' }} />
+                        </Avatar>
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Typography
+                          sx={{
+                            fontSize: '1.3rem',
+                            fontWeight: 500,
+                            color: theme.palette.primary[200]
+                          }}
+                        >
+                          Vibration
+                        </Typography>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Box>
+        </CardWrapper>
+      )}
+    </>
+  );
+};
+
+TotalOrderLineChartCard.propTypes = {
+  isLoading: PropTypes.bool,
+  vibration: PropTypes.number
+};
+
+export default TotalOrderLineChartCard;
